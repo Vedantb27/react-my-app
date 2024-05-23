71f007def64df11f75627a684109631e1b208666
Vedantb27 <157893464+Vedantb27@users.noreply.github.com>
2024-05-23 13:32:25 +0530

    Getting the Data from Api instead of local object succesfully

--- src/MyComponents/Category/Categorymain.js
+++ src/MyComponents/Category/Categorymain.js
@@ -1,125 +1,25 @@
 import React from "react";
 import { Cards } from "./Cards";
 import { Categorylist } from "./Categorylist";
+import { useState, useEffect } from "react";
 
 export const Categorymain = () => {
   /* JSON Data*/
-  let categoryData = {
-    "Veg": [
-      {
-        "cardId": 1,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597",
-        "title": "Food 1",
-        "type": "Veg"
-      },
-      {
-        "cardId": 2,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/490629b70f89da8a5b93fc199ece335e",
-        "title": "Food 2",
-        "type": "Veg"
-      }, {
-        "cardId": 3,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/490629b70f89da8a5b93fc199ece335e",
-        "title": "Food 3",
-        "type": "Veg"
-      },{
-        "cardId": 4,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/490629b70f89da8a5b93fc199ece335e",
-        "title": "Food 4",
-        "type": "Veg"
-      }
-    ],
-    "Non-Veg": [
-      {
-        "cardId": 3,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/63178e3e64d503a479f2a2048a474552",
-        "title": "Food 3",
-        "type": "Veg"
-      },
-      {
-        "cardId": 4,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/20/349e8d46-3138-4b19-96b2-df400a77e285_622197.JPG",
-        "title": "Food 4",
-        "type": "Non Veg"
-      }
-    ],
-    "Curi": [
-      {
-        "cardId": 5,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/b5c7e325-a2b3-4334-b104-0b20df81dd93_23728.JPG",
-        "title": "Food 5",
-        "type": "Veg"
-      },
-      {
-        "cardId": 6,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/b5c7e325-a2b3-4334-b104-0b20df81dd93_23728.JPG",
-        "title": "Food 6",
-        "type": "Veg"
-      }
-    ],
-    "ld": [
-      {
-        "cardId": 7,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/19/741d7d41-1341-4358-b6e0-cc22b8e82f9a_750391.JPG",
-        "title": "Food 7",
-        "type": "Non Veg"
-      },
-      {
-        "cardId": 8,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/6e44fd7f1e5cd9967edfe47c10247671",
-        "title": "Food 8",
-        "type": "Veg"
-      }
-    ],
-    "chiken": [
-      {
-        "cardId": 9,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/69a061b7e0f951cef2b4947946f94ec6",
-        "title": "Food 9",
-        "type": "Veg"
-      },
-      {
-        "cardId": 10,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/85ccae4e3576f9330af102c46ca85395",
-        "title": "Food 10",
-        "type": "Non Veg"
-      }
-    ],
-    "Category 6": [
-      {
-        "cardId": 11,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/64fd45fd9f44c1737bc446e470bed666",
-        "title": "Food 11",
-        "type": "Veg"
-      },
-      {
-        "cardId": 12,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/63178e3e64d503a479f2a2048a474552",
-        "title": "Food 12",
-        "type": "Veg"
-      }
-    ],
-    "Vegx": [
-      {
-        "cardId": 13,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf",
-        "title": "Food 13",
-        "type": "Non Veg"
-      },
-      {
-        "cardId": 14,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/91afd7c438fcf1a3b55b97c2fbaa1cfa",
-        "title": "Food 14",
-        "type": "Veg"
-      },
-      {
-        "cardId": 15,
-        "imageId": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/5/19/741d7d41-1341-4358-b6e0-cc22b8e82f9a_750391.JPG",
-        "title": "Food 15",
-        "type": "Veg"
-      }
-    ]
+ const [categoryData , setcategoryData] = useState({});
+
+
+ useEffect(()=>{
+  const fetchData = async () =>{
+    try{
+      const response = await fetch("https://mocki.io/v1/54bd242a-076b-474b-915a-0362ba8f10c5");
+      const data = await response.json();
+      setcategoryData(data);
+    }catch (error){
+      console.log("error fetching the data",error)
+    }
   }
+  fetchData()
+ },[])
 
   
   return ( 
