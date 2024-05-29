11ce557ca4243b0cdde3253ec2378c6bd665d7ce
Vedantb27 <157893464+Vedantb27@users.noreply.github.com>
2024-05-29 22:13:33 +0530

    routing testing Done for temprary without mail vaerification

--- src/MyComponents/Nabar/Loginform.js
+++ src/MyComponents/Nabar/Loginform.js
@@ -1,7 +1,9 @@
 import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Loginform = ({onClose}) => {
     const LoginformRef = useRef();
+    const navigate = useNavigate();
     
     const closeLoginform = (e)=>{
       if(LoginformRef.current === e.target){
@@ -71,7 +73,7 @@ export const Loginform = ({onClose}) => {
               </a>
             </div>
           </div>
-          <button
+          <button onClick={navigate("/Admineditcategory")}
             type="submit"
             class="w-full px-3 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-500"
           >
